ea057e4f492a4f2a364e11a9422a50856d19e4bc
hexoul <crosien@gmail.com>
2018-11-12 11:09:41 +0900

    update go-coinmarketcap version and add testunit for kucoin

--- main_test.go
+++ main_test.go
@@ -4,10 +4,21 @@ import (
 	"testing"
 	"time"
 
+	kucoin "github.com/eeonevision/kucoin-go"
+
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
+
+var (
+	testAccessKey = map[string]string{"kucoin": "YOUR_ACCESS_KEY"}
+	testSecretKey = map[string]string{"kucoin": "YOUR_SECRET_KEY"}
+)
+
+func init() {
+
+}
 
 func TestGit(t *testing.T) {
 	r, err := git.PlainOpen("./")
@@ -36,5 +47,19 @@ func TestGit(t *testing.T) {
 		},
 	}); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestKucoinBalance(t *testing.T) {
+	k := kucoin.New(testAccessKey["kucoin"], testSecretKey["kucoin"])
+	if _, err := k.GetCoinBalance("BTC"); err != nil {
+		t.FailNow()
+	}
+}
+
+func TestKucoinListMergedDealtOrders(t *testing.T) {
+	k := kucoin.New(testAccessKey["kucoin"], testSecretKey["kucoin"])
+	if _, err := k.ListMergedDealtOrders("ETH-BTC", "BUY", 20, 1, 0, 0); err != nil {
+		t.FailNow()
 	}
 }
