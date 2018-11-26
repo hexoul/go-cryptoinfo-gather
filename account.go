9bd949ad0edcad474d51d9ee493bbe7298ab6415
hexoul <crosien@gmail.com>
2018-11-26 15:58:16 +0900

    account: imp. getKucoinBalnace()

--- account.go
+++ account.go
@@ -1,19 +1,61 @@
 package main
 
-func getKucoinBalnace() (meta, eth, btc float64) {
-	if bal, err := clients.kucoin.GetCoinBalance("META"); err == nil {
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	kucoin "github.com/eeonevision/kucoin-go"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	balanceLogger *log.Logger
+)
+
+func init() {
+	// Initialize logger
+	balanceLogger = log.New()
+
+	// Default configuration
+	balanceLogger.Formatter = &log.JSONFormatter{
+		TimestampFormat: time.RFC3339,
+	}
+	if f, err := os.OpenFile("./balance.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666); err == nil {
+		balanceLogger.Out = io.MultiWriter(f, os.Stdout)
+	} else {
+		fmt.Print("Failed to open log file: you can miss important log")
+		balanceLogger.Out = os.Stdout
+	}
+	balanceLogger.SetLevel(log.InfoLevel)
+}
+
+func getKucoinBalnace(k *kucoin.Kucoin) (meta, eth, btc float64) {
+	if k == nil {
+		return
+	}
+	if bal, err := k.GetCoinBalance("META"); err == nil {
 		meta = bal.Balance + bal.FreezeBalance
 	}
-	if bal, err := clients.kucoin.GetCoinBalance("ETH"); err == nil {
+	if bal, err := k.GetCoinBalance("ETH"); err == nil {
 		eth = bal.Balance + bal.FreezeBalance
 	}
-	if bal, err := clients.kucoin.GetCoinBalance("BTC"); err == nil {
+	if bal, err := k.GetCoinBalance("BTC"); err == nil {
 		btc = bal.Balance + bal.FreezeBalance
 	}
+
+	balanceLogger.WithFields(log.Fields{
+		"exchange": "kucoin",
+		"meta":     meta,
+		"eth":      eth,
+		"btc":      btc,
+	}).Info("GatherBalance")
 	return
 }
 
 // GetBalances records balances
-func GetBalances() {
-	getKucoinBalnace()
+func GetBalances(c *Clients) {
+	getKucoinBalnace(c.kucoin)
 }
---
+++ account_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetBalances(t *testing.T) {
+	GetBalances(&testClients)
+}
