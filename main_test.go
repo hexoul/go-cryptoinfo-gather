91607f31663e687dbfe15d69c5058a2bf79cc781
hexoul <crosien@gmail.com>
2019-02-19 03:15:18 +0900

    log updated

--- main_test.go
+++ main_test.go
@@ -8,17 +8,17 @@ import (
 
 var (
 	testAccessKey = map[string]string{
-		"kucoin":    "YOUR_ACCESS_KEY",
+		"kucoin":    "5c6ac76a1cde7830729e25eb",
 		"coinsuper": "YOUR_ACCESS_KEY",
 		"abcc":      "YOUR_ACCESS_KEY",
 	}
 	testSecretKey = map[string]string{
-		"kucoin":    "YOUR_SECRET_KEY",
+		"kucoin":    "5c0910e-b3ad-41fe-af34-0164d9143322",
 		"coinsuper": "YOUR_SECRET_KEY",
 		"abcc":      "YOUR_SECRET_KEY",
 	}
 	testPassPhrase = map[string]string{
-		"kucoin": "YOUR_PASSPHRASE",
+		"kucoin": "MetadiumToTheMoon",
 	}
 
 	testClients Clients
